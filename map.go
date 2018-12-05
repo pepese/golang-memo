f9321ae85d1b45da12d33d445dd63aacb176816d
pepese <tanakakns@tanakakensukenoMacBook-puro.local>
2018-12-05 11:55:15 +0900

    update

---
+++ map.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m1 := map[string]int{"a": 1, "b": 2}
+	fmt.Println(m1)
+	fmt.Println(m1["a"])
+
+	// キーの存在確認
+	value, exist := m1["a"]
+	fmt.Print(value)
+	fmt.Print(", ")
+	fmt.Println(exist)
+
+	// 要素削除
+	delete(m1, "b")
+	fmt.Println(m1)
+
+	m2 := make(map[string]int)
+	fmt.Println(m2)
+	m2["c"] = 3
+	fmt.Println(m2)
+}
