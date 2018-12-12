a844e4ba850b5fe5b4a25981dd9f845aa15a41a4
pepese <tanakakns@tanakakensukenoMacBook-puro.local>
2018-12-12 20:03:14 +0900

    update

--- 02_algorithm/01_sort.go
+++ 02_algorithm/01_sort.go
@@ -27,71 +27,83 @@ func bubbleSort(list []int) (int, error) {
 	return roopNum, nil
 }
 
-func quickSort(list []int, left int, right int) (int, error) {
-	roopNum := 0
+func quickSort(list []int) (int, error) {
+	roop := 0
+	var roopNum *int
+	roopNum = &roop
 	if list == nil {
-		return roopNum, errors.New("")
+		return *roopNum, errors.New("")
 	} else if len(list) < 2 {
-		return roopNum, nil
+		return *roopNum, nil
 	}
-	pivot := func() int {
-		num1, num2, num3 := list[left], list[int((left+right)/2)], list[right]
-		if num1 > num2 {
-			if num1 < num3 {
-				return num1
-			} else if num2 > num3 {
-				return num2
-			} else {
-				return num3
-			}
-		} else {
-			if num2 < num3 {
-				return num2
-			} else if num1 > num3 {
-				return num1
-			} else {
-				return num3
-			}
-		}
-	}()
-	i, j := left, right
-	if right > left {
-		for true {
-			for list[i] <= pivot {
+	var calcQuick func(list []int, left int, right int, roop *int)
+	calcQuick = func(list []int, left int, right int, roop *int) {
+		if left < right {
+			i, j := left, right
+			pivot := func(num1 int, num2 int, num3 int) int {
+				if num1 > num2 {
+					if num1 < num3 {
+						return num1
+					} else if num2 > num3 {
+						return num2
+					} else {
+						return num3
+					}
+				} else {
+					if num2 < num3 {
+						return num2
+					} else if num1 > num3 {
+						return num1
+					} else {
+						return num3
+					}
+				}
+			}(list[left], list[int(i+(j-i)/2)], list[right])
+			for true {
+				for list[i] < pivot {
+					i++
+				}
+				for pivot < list[j] {
+					j--
+				}
+				*roop++
+				if i >= j {
+					break
+				}
+				tmp := list[i]
+				list[i] = list[j]
+				list[j] = tmp
 				i++
-			}
-			for list[j] > pivot {
 				j--
 			}
-			roopNum++
-			if i >= j {
-				break
-			}
-			tmp := list[i]
-			list[i] = list[j]
-			list[j] = tmp
+			calcQuick(list, left, i-1, roop)
+			calcQuick(list, j+1, right, roop)
 		}
-		roop1, _ := quickSort(list, left, i-1)
-		roop2, _ := quickSort(list, j+1, right)
-		roopNum += roop1 + roop2
 	}
-	return roopNum, nil
+	calcQuick(list, 0, len(list)-1, roopNum)
+	return *roopNum, nil
 }
 
 func main() {
-	list := []int{}
+	list1 := []int{}
+	list2 := []int{}
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 10; i++ {
-		list = append(list, rand.Intn(10))
+		rnd := rand.Intn(10)
+		list1 = append(list1, rnd)
+		list2 = append(list2, rnd)
 	}
-	fmt.Println("before sort ", list)
-	/*
-		num, err := bubbleSort(list)
-		if err == nil {
-			fmt.Println("after bubbleSort  ", list, num, err)
-		} else {
-			fmt.Println(err)
-		}*/
-	num, _ := quickSort(list, 0, len(list)-1)
-	fmt.Println("after sort  ", list, num)
+	fmt.Println("before sort ", list1)
+	num, err := bubbleSort(list1)
+	if err == nil {
+		fmt.Println("after bubbleSort  ", list1, num, err)
+	} else {
+		fmt.Println(err)
+	}
+	num, err = quickSort(list2)
+	if err == nil {
+		fmt.Println("after quickSort  ", list2, num, err)
+	} else {
+		fmt.Println(err)
+	}
 }
