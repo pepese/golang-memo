fa3ea82989a1bbfe7ee1b47405348e4ce803c482
pepese <tanakakns@tanakakensukenoMacBook-Pro.local>
2019-02-09 12:04:04 +0900

    update

---
+++ now.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sort(list []int) error {
+	eNum := len(list)
+	for i := eNum; i > 0; i-- {
+		for j := 0; j < i-1; j++ {
+			if list[j] > list[j+1] {
+				temp := list[j]
+				list[j] = list[j+1]
+				list[j+1] = temp
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	list := []int{3, 4, 1, 2, 8, 5}
+	sort(list)
+	fmt.Println(list)
+}
