d70d5e79eb786c1bcf89461d8697986b649f68f0
pepese <tanakakns@tanakakensukenoMacBook-Pro.local>
2019-03-02 09:58:09 +0900

    update

--- 01_grammar/02_pointer.go
+++ 01_grammar/02_pointer.go
@@ -4,14 +4,34 @@ import (
 	"fmt"
 )
 
+func sample(pa *int) {
+	fmt.Println("---func sample start")
+	fmt.Println(pa)
+	fmt.Println(*pa)
+	fmt.Println(&pa) // ポインタを格納しているアドレスは main と変わっている
+	fmt.Println("---change value")
+	*pa = 3
+	fmt.Println(pa)
+	fmt.Println(*pa)
+	fmt.Println(&pa)
+	fmt.Println("---func sample end")
+}
+
 func main() {
 	a := 5
 
 	var pa *int
 	pa = &a
 
+	fmt.Println(pa)  // ポインタが参照しているアドレス：0xc000016078
+	fmt.Println(*pa) // アドレスに格納している値：5
+	fmt.Println(&a)  // a のアドレス（paと同じ）：0xc000016078
+	fmt.Println(&pa) // ポインタを格納しているアドレス：0xc00000c028
+
+	fmt.Println("---func sample call")
+	sample(pa)
+
 	fmt.Println(pa)
 	fmt.Println(*pa)
-	fmt.Println(&a)
 	fmt.Println(&pa)
 }
--- 98_atcoder/ABC049C/atcoder.go
+++ 98_atcoder/ABC049C/atcoder.go
@@ -1,5 +1,7 @@
 package main
 
+//https://qiita.com/drken/items/fd4e5e3630d0f5859067
+
 import (
 	"bufio"
 	"fmt"
@@ -8,21 +10,29 @@ import (
 	"strings"
 )
 
-var (
-	yes = "YES"
-	no  = "NO"
-)
-
 func search(s string, words []string, index int, result *string) {
 	for _, w := range words {
 		if index < len(s) && strings.Index(s[index:], w) == 0 {
+			fmt.Printf("s:%s, index:%d, w:%s, now:%s\n", s, index, w, s[index:])
 			if index+len(w) == len(s) {
-				result = &yes
+				*result = "YES"
 			} else {
 				search(s, words, index+len(w), result)
 			}
 		}
-		fmt.Printf("%s, w: %s, index: %d, result: %s\n", s[index:], w, strings.Index(s[index:], w), *result)
+	}
+}
+
+func search2(s string, words []string, result *string) {
+	for _, w := range words {
+		if strings.LastIndex(s, w) == len(s)-len(w) {
+			fmt.Printf("s:%s, w:%s\n", s, w)
+			if len(w) == len(s) {
+				*result = "YES"
+			} else if len(s) > len(w) {
+				search2(s[0:len(s)-len(w)], words, result)
+			}
+		}
 	}
 }
 
@@ -33,34 +43,11 @@ func exec(r io.Reader) string {
 	s := sc.Text()
 	/*
 		index, result := 0, "NO"
-		for true {
-			flag := false
-			for i, w := range words {
-				if index < len(s) && strings.Index(s[index:], w) == 0 {
-					if index+len(w) == len(s) {
-						result = "YES"
-					}
-					if i == 0 && (strings.Index(s[index+5:], words[2]) == 0 || strings.Index(s[index+5:], words[3]) == 0) {
-						continue
-					} else {
-						index += len(w)
-						break
-					}
-				} else if i == 3 && !flag {
-					flag = true
-				}
-			}
-			if result == "YES" || flag {
-				break
-			}
-		}
-		return result
+		search(s, words, index, &result)
 	*/
-	index := 0
-	var result *string
-	result = &no
-	search(s, words, index, result)
-	return *result
+	result := "NO"
+	search2(s, words, &result)
+	return result
 }
 
 func main() {
