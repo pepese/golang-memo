09284f326339031c7ab256346d2e68f3a3b16744
田中健介 <tanakakns@tanakakensukenoMacBook-puro.local>
2019-03-06 13:50:25 +0900

    update

--- 02_algorithm/03_bit_full_search.go
+++ 02_algorithm/03_bit_full_search.go
@@ -9,6 +9,11 @@ func main() {
 	}
 	fmt.Println("---")
 	for bit := 1 << (n - 1); bit < (1 << n); bit++ { // n ビットの組合せ
-		fmt.Printf("%b\n", bit)
+		fmt.Printf("%b", bit)
+		if (bit & (1 << (2 - 1))) > 0 { // 2 ビット目が立っているか確認
+			fmt.Println(", 2nd bit flaged.")
+		} else {
+			fmt.Println()
+		}
 	}
 }
